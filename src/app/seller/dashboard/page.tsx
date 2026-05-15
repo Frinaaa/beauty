e3abab101147729dad55d230e100daa23ee2a548
frina <frinafrin149@gmail.com>
2026-05-15 23:42:08 +0530

    1st

---
+++ src/app/seller/dashboard/page.tsx
@@ -0,0 +1,14 @@
+export default function Home() {
+
+  return (
+
+    <div className="h-screen flex items-center justify-center bg-[#F8F7F4]">
+
+      <h1 className="text-5xl font-light tracking-[5px] text-[#5B2333]">
+        BEAUTY SELLER
+      </h1>
+
+    </div>
+
+  );
+}
